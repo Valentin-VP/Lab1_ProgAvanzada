211f792bf39417ce2caf12c8847891fa1eab48c7
rcastro33 <rcastro@programmer.net>
2020-03-29 21:44:24 -0300

    Controles de agregarVehiculo. Manejo de excepciones en registrarUsuario (existeUsuario)

--- main.cpp
+++ main.cpp
@@ -16,15 +16,27 @@
 
 using namespace std;
 #define MAX_USER 30
+#define MAX_VEHICULOS 100
 
 struct{
 	Usuario* usuarios[MAX_USER];
 	int tope;
 } coleccionUsuarios;
 
+struct{
+	Vehiculo* vehiculos[MAX_VEHICULOS];
+	int tope;
+} coleccionVehiculos;
+
 Usuario* obtenerUsuario (string);
 void verViajesAntesDeFecha();
 DtViaje** verViajesAntesDeFecha(DtFecha&,string,int&);
+void agregarVehiculo();
+void agregarVehiculo(DtVehiculo&);
+void existeVehiculo(int);
+void porcentajeValido(float);
+void precioBaseValido(float);
+
 
 Usuario* obtenerUsuario(string ci){ //falta probar
     Usuario* user;
@@ -40,32 +52,78 @@ Usuario* obtenerUsuario(string ci){ //falta probar
 	return user;
 }
 
-
-void registrarUsuario(){ //funcionando
-    string ci, nombre;
-    cout << "Cedula: ";
-    cin >> ci;
-    bool existe=false;
+void existeUsuario(string ci){
     int i=0;
+    bool existe=false;
     while((!existe)&&(i<coleccionUsuarios.tope)){
         if(ci==coleccionUsuarios.usuarios[i]->getCedula())
             existe=true;
         else
             i++;
-    }
-    if(existe){
-        cout << "Usuario ya registrado" << endl;
-    }else{    
+        }
+        if(existe)
+            throw invalid_argument("ERROR: YA EXISTE USUARIO CON ESA CI EN EL SISTEMA\n");
+}
+
+void registrarUsuario(){ //funcionando ***FALTA MANEJO DE EXCEPCION USANDO TRY CATCH***
+    string ci, nombre;
+    cout << "Cedula: ";
+    cin >> ci;
+    try{
+        existeUsuario(ci);
         cout << "Nombre: ";
         cin >> nombre;
-        DtFecha* fecha = new DtFecha(27,3,2020);
+        DtFecha* fecha = new DtFecha(27,3,2020); //tomar fecha del sistema o que?
         Usuario* u=new Usuario(ci,nombre,*fecha);
         coleccionUsuarios.usuarios[coleccionUsuarios.tope] = u;
         coleccionUsuarios.tope++;
+    }catch (invalid_argument& e){
+        cout << e.what() << endl;
     }
 }
  
- 
+void agregarVehiculo(){
+    system("clear");
+    //Interfaz con el usuario
+    	int nroSerie;
+		float porcentajeBateria;
+		float precioBase;
+        DtVehiculo vehiculo; //REVISAR
+        //Controles requeridos: 1-No existe un vehiculo con el mismo numero de serie
+    try{
+        existeVehiculo(nroSerie);
+        // 2 - Porcentaje en valor entre 0 y 100
+        porcentajeValido(porcentajeBateria);
+        // 3 - Precio base positivo
+        precioBaseValido(precioBase);
+        // Se agrega vehiculo a coleccionVehiculos
+        
+    }catch (invalid_argument& e){
+        //YA EXISTE EL VEHICULO, PORCENTAJE INVALIDO O 
+        cout << e.what() << endl;
+    }
+}
+
+void precioBaseValido(float pb){
+    if(pb<=0)
+        throw invalid_argument("Precio base inválido; debe ser mayor a 0\n");
+}
+void porcentajeValido(float pb){
+    if((pb<0)||(pb>100))
+        throw invalid_argument("Valor de batería inválido\n");
+}
+void existeVehiculo(int nroSerie){
+    int i=0;
+    // bool existe=true;
+    while(i<coleccionVehiculos.tope){//&&(existe)){
+        if(nroSerie==coleccionVehiculos.vehiculos[i]->getNroSerie())
+            throw invalid_argument("Ya existe ese vehículo\n");
+        i++;
+    }
+}
+
+
+
 // DtViaje ---> Rodrigo -en proceso-
 // Bloque de codigo cuando se solicita imprimir Viajes -- OPERACION verViajesAntesDe
 void verViajesAntesDeFecha(){
@@ -117,8 +175,9 @@ DtViaje** verViajesAntesDeFecha (DtFecha& fecha, string ci, int& cantViajes){
             }
         }
         for(int i=0; i<user->getTopeViajes;i++){
-            if(viajes[i]->getFecha()<fecha)
+            if(viajes[i]->getFecha()<fecha){
 
+            }
         }
 
 
@@ -129,4 +188,6 @@ DtViaje** verViajesAntesDeFecha (DtFecha& fecha, string ci, int& cantViajes){
 int main(){
     //Llamada a operacion D desde el main.
     verViajesAntesDeFecha();
+    //Llamada a operacion B desde el main.
+    agregarVehiculo();
 }
